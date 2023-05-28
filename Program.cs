e6113a66acdb4611e0919876d2ac8eb4f4e1e7a7
Jean Marques <jmichelmarques21@gmail.com>
2023-05-28 19:42:13 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TesteListasemCsharp {
+    class Program {
+        static void Main(string[] args) {
+
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("List count: " + list.Count);
+
+
+
+        }
+    }
+}
