67d3f85812ce12c82e152fba5d825207d7473302
Jean Marques <jmichelmarques21@gmail.com>
2023-05-29 00:39:08 -0300

    a

--- Program.cs
+++ Program.cs
@@ -16,9 +16,34 @@ namespace TesteListasemCsharp {
             foreach (string obj in list) {
                 Console.WriteLine(obj);
             }
-
+            Console.WriteLine();
             Console.WriteLine("List count: " + list.Count);
-
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position 'A': " + pos1);
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position 'A': " + pos2);
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------");
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine();
+            foreach (string obj in list2) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("--------------------------------");
+            list.Remove("Alex");
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("--------------------------------");
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (string obj in list) {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("--------------------------------");
 
 
         }
